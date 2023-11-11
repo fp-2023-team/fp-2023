a8066eba6bed7b29c581d74d10af1faa9e16ee80
AluminumAlman <Almantas1122334455@gmail.com>
2023-11-11 22:27:53 +0200

    Add file reading/writing + import JSON dependency

--- src/Lib3.hs
+++ src/Lib3.hs
@@ -1,34 +1,43 @@
 {-# LANGUAGE DeriveFunctor #-}
 
 module Lib3
-  ( executeSql,
-    Execution,
-    ExecutionAlgebra(..)
-  )
+    (
+        executeSql,
+        Execution,
+        ExecutionAlgebra(..)
+    )
 where
 
 import Control.Monad.Free (Free (..), liftF)
 import DataFrame (DataFrame)
+import InMemoryTables ( tableEmployees )
 import Data.Time ( UTCTime )
+import Text.JSON.Generic
 
 type TableName = String
-type FileContent = String
+type TableContent = String
 type ErrorMessage = String
 
 data ExecutionAlgebra next
-  = LoadFile TableName (FileContent -> next)
-  | GetTime (UTCTime -> next)
-  -- feel free to add more constructors here
-  deriving Functor
+    = GetTime (UTCTime -> next)
+    | SaveTable TableName TableContent (() -> next)
+    | LoadTable TableName (TableContent -> next)
+    -- feel free to add more constructors here
+    deriving Functor
 
 type Execution = Free ExecutionAlgebra
-
-loadFile :: TableName -> Execution FileContent
-loadFile name = liftF $ LoadFile name id
 
 getTime :: Execution UTCTime
 getTime = liftF $ GetTime id
 
+saveTable :: TableName -> TableContent -> Execution ()
+saveTable name content = liftF $ SaveTable name content id
+
+loadTable :: TableName -> Execution TableContent
+loadTable name = liftF $ LoadTable name id
+
 executeSql :: String -> Execution (Either ErrorMessage DataFrame)
 executeSql sql = do
     return $ Left "implement me"
+    a <- loadTable "foo"
+    return $ Left a
