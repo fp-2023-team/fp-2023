ccb9240d082c9346bbb0d76e5124c07b2974b28b
AluminumAlman <Almantas1122334455@gmail.com>
2023-12-22 14:48:10 +0200

    executeStatement DropTable: exclude dropped table

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1052,7 +1052,9 @@ executeStatement (CreateTableStatement tablename' columns') database' = do
 executeStatement (DropTableStatement tablename') database' = do
     _ <- guardCheck (null $ lookup tablename' database')
         $ "Table by name " ++ tablename' ++ " does not exist"
-    executeStatement (ShowTableStatement Nothing) database'
+    executeStatement
+        (ShowTableStatement Nothing)
+        [table | table@(tableName'', _) <- database', tableName'' /= tablename']
 executeStatement _ _ = Left $ "Unknown unsupported statement"
 
 nullOrAny :: (Foldable t) => (a -> Bool) -> t a -> Bool
